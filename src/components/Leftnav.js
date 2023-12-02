f3c248418f9750318f69859a620502c0f4209604
Cibiyanna <cibiyanna.p2022cse@sece.ac.in>
2023-12-03 00:31:24 +0530

    left side completed

--- src/components/Leftnav.js
+++ src/components/Leftnav.js
@@ -1,21 +1,63 @@
-import React from 'react'
+import React  from 'react'
 import temp_logo from '../assets/temp_logo.jpeg';
+import { Link } from 'react-router-dom';
+
+
+
 const Leftnav = () => {
   return <aside className='h-[100vh] bg-white'>
     <div className='top'>
       {/* logo  */}
       <div className='logo'>      
-        <img src={temp_logo} alt='image' className='w-[50px] h-[50px]'></img>
-        <h1 className='text-3xl font-bold'> <span className='text-orange-600'>EFFI</span> TRACK</h1>
+        <img src={temp_logo} alt='image' className='w-[40px] h-[40px]'></img>
+        <h1 className='text-2xl font-semibold'> <span className='text-orange-600'>EFFI</span> TRACK</h1>
       </div>
         {/* close button on small screen */}
       <div className='close'>
         <span class="material-icons-sharp">close</span>
       </div>
-        
     </div>
-    <div className='mid'>
-
+    <div className='mid '>
+        <div className='mid_in'>
+          <a href="#" className=''> 
+            <span class="material-icons-sharp">dashboard</span>
+           <h4>DashBoard</h4> 
+          </a>
+          <a href="#" className='active'> 
+          <span class="material-icons-sharp">dashboard</span>
+          <h4>LeetCode</h4> 
+          </a>
+          <a href="#" className=''>
+          <span class="material-icons-sharp">dashboard</span> 
+          <h4>CodeForces</h4>
+          </a>
+          <a href="#" className=''> 
+          <span class="material-icons-sharp">settings</span>
+          <h4> CodeChef</h4>
+          </a>
+          <a href="#" className=''> 
+          <span class="material-icons-sharp">settings</span> 
+          <h4>Github </h4>
+          </a>
+          <div className='aside-bottom'>
+            <div className='settings'>
+              <a href="#" className=''> 
+                <span class="material-icons-sharp">settings</span> 
+                <h4>Settings </h4>
+              </a>
+              <a href="#" className=''> 
+                <span class="material-icons-sharp">message</span> 
+                <h4>Support </h4>
+              </a>
+            </div>
+            <div className='sign-out'>
+              <a href="#" className=''> 
+                <span class="material-icons-sharp">dashboard</span> 
+                <h4>Github </h4>
+              </a>
+            </div>
+          </div>
+        </div>
     </div>
     <div className='bottom'>
       
