63b640734b111fcd0f6409491c09ad4b4195e8a7
muruga21 <murugaperumal21062004@gmail.com>
2023-12-02 21:58:01 +0530

    added basic components

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Leftnav from './Leftnav'
+import Main from './Main'
+
+const Home = () => {
+  return (
+    <>
+        <Leftnav/>
+        <Main/>
+    </>
+  )
+}
+
+export default Home
