269cfb8566eb847edbbbf102865cab1ac0b99b2e
Cibiyanna <cibiyanna.p2022cse@sece.ac.in>
2023-12-02 22:46:43 +0530

    icon and some changes in index.css

--- src/components/Leftnav.js
+++ src/components/Leftnav.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import temp_logo from '../assets/temp_logo.jpeg';
 const Leftnav = () => {
   return <aside className='h-[100vh] bg-white'>
     <div className='top'>
       {/* logo  */}
       <div className='logo'>      
-        <img src=''alt='image'></img>
-        <h1 className='text-3xl font-bold'>EffiTrack</h1>
+        <img src={temp_logo} alt='image' className='w-[50px] h-[50px]'></img>
+        <h1 className='text-3xl font-bold'> <span className='text-orange-600'>EFFI</span> TRACK</h1>
       </div>
         {/* close button on small screen */}
       <div className='close'>
