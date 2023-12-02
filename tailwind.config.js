b2495ee073527fa5664cd7cc2776d2127370ca44
Cibiyanna <cibiyanna.p2022cse@sece.ac.in>
2023-12-02 22:30:28 +0530

    tailwind theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,34 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: 'Orbitron',
+      secondary: 'Rajdhani',
+      tertiary: 'Aldrich',
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#0a0a0a',
+        accent: '#B809C3',
+        cardbg:'#EEEDED',
+        cardcl:"#27374D",
+        tech:"#526D82",
+        ml:'#009FBD',
+        django:"#1B9C85"
+      },
+    },
   },
-  plugins: [],
+    plugins:[],
 }
 
