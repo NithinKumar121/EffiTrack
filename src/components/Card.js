4c90fb98f46458fdc1fd93e55ae9b96fabb27b90
muruga21 <murugaperumal21062004@gmail.com>
2023-12-03 19:36:18 +0530

    added card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,28 @@
-const Card = () =>{
-    return(
-        <>
-        </>
-    )
+import React from 'react'
+
+const Card = ({card}) => {
+    console.log(card);
+  return (
+    //template literal
+    <div className={`card ${card.sub_title}`}>      
+        <div className="card-details">
+            <div className="card-details-top bg-red-400 rounded-xl">
+                <h1>{card.title}</h1>
+            </div>
+            <h1>{card.sub_title}</h1>
+            <h3>Count : {card.count}</h3>
+            <h4>Rank : {card.rank}</h4>
+        </div>
+        <div class="progress">
+                <svg >
+                    <circle cx="38" cy="38" r="36"></circle>
+                </svg>
+                <div class="number">
+                    <p>{card.comp_percent}</p>
+                </div>
+        </div>
+    </div>
+  )
 }
+
+export default Card
--- src/components/Mid.js
+++ src/components/Mid.js
@@ -1,62 +1,41 @@
+import Card from "./Card";
+
 const Mid = ()=>{
+    const cardData = [
+        {
+            title:'Leetcode',
+            sub_title:'leetcode',
+            count:'340',
+            rank:'49+',
+            comp_percent:'80%'
+        },
+        {
+            title:'CodeForces',
+            sub_title:'codechef',
+            count:'340',
+            rank:'49+',
+            comp_percent:'56%'
+        },
+        {
+            title:'CodeForces',
+            class:'codeforces',
+            sub_title:'leetcode',
+            count:'340',
+            rank:'49+',
+            comp_percent:'70%'
+        }
+    ]
     return(
         <section className="mid-top mt-[1rem]">
             <div className="mid-left">
                 <div className="cards">
-                    <div className="card card1">        
-                        <div className="card-details">
-                            <div className="card-details-top bg-red-400 rounded-xl">
-                                <h1>LeetCode</h1>
-                            </div>
-                            <h1>LeetCode</h1>
-                            <h3>Count : 340</h3>
-                            <h4>Rank : 49+</h4>
-                        </div>
-                        <div class="progress">
-                                <svg >
-                                    <circle cx="38" cy="38" r="36"></circle>
-                                </svg>
-                                <div class="number">
-                                    <p>81%</p>
-                                </div>
-                        </div>
-                    </div>
-                    <div className="card card2"> 
-                        <div className="card-details">
-                            <div className="card-details-top bg-orange-300 rounded-xl">
-                                <h1>CodeChef</h1>
-                            </div>
-                            <h1>LeetCode</h1>
-                            <h3>Count : 340</h3>
-                            <h4>Rank : 49+</h4>
-                        </div>
-                        <div class="progress">
-                                <svg >
-                                    <circle cx="38" cy="38" r="36"></circle>
-                                </svg>
-                                <div class="number">
-                                    <p>56%</p>
-                                </div>
-                        </div>
-                    </div>
-                    <div className="card card3"> 
-                        <div className="card-details">
-                            <div className="card-details-top bg-green-500 rounded-xl">
-                                <h1>CodeForces</h1>
-                            </div>
-                            <h1>LeetCode</h1>
-                            <h3>Count : 340</h3>
-                            <h4>Rank : 49+</h4>
-                        </div>
-                        <div class="progress">
-                                <svg >
-                                    <circle cx="38" cy="38" r="36"></circle>
-                                </svg>
-                                <div class="number">
-                                    <p>70%</p>
-                                </div>
-                        </div>
-                    </div>
+                    {
+                        cardData.map((card,index)=>{
+                            return(
+                                <Card card={card}/>
+                            );
+                        })
+                    }
                 </div>
             </div>
             <div className="mid-right">
