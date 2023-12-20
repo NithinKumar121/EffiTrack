d124a66b8e7918f1d7fc60596320ca550a82d018
Cibiyanna <pkcibiyanna@gmail.com>
2023-12-20 19:38:15 +0530

    codeforcesModel created with the available data

---
+++ backend/models/codeFrocesModel.js
@@ -0,0 +1,31 @@
+const {Schema,model} = require('mongoose');
+
+const codefroceSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    contribution:{
+        type:Number,
+        default:0,
+    },
+    contest: {
+        type: [
+            {
+                contestId: Number,
+                contestName: String,
+                handle: String,
+                rank: Number,
+                ratingUpdateTimeSeconds: Number,
+                oldRating: Number,
+                newRating: Number,
+            }
+        ],
+        default: [],
+    },
+})
+
+const codeforceModel = new model('codeforceSchema',codefroceSchema);
+
+module.exports = codeforceModel;
