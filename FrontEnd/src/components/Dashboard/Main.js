ce91853bf61610e17c65ee02f44de9b8b1f77bf3
Cibiyanna <pkcibiyanna@gmail.com>
2023-12-25 14:31:50 +0530

    modified nav

--- FrontEnd/src/components/Dashboard/Main.js
+++ FrontEnd/src/components/Dashboard/Main.js
@@ -7,12 +7,15 @@ import Calender from './Calender'
  const Main = () => {
   return (
     <div className='overflow-hidden h-full px-3 py-2'>
-      <div className='overflow-y-auto w-full h-full no-scrollbar '>
-        <Navbar/>
-        <Shimmer/>
-        <Showcase/>
-      </div>
-       
+        <Navbar className='fixed'/>
+        <div className='overflow-hidden h-[90vh] mt-2 p-2'>
+          <div className='overflow-y-auto w-full h-full no-scrollbar'>
+            <Mid/>
+            <Showcase/>
+            <Showcase/>
+            <Showcase/>
+          </div>
+        </div>       
     </div>
   )
 }
--- FrontEnd/src/components/Dashboard/Navbar.js
+++ FrontEnd/src/components/Dashboard/Navbar.js
@@ -5,7 +5,7 @@ import Theme from './Theme';
 const Navbar = () => {
 
   return (
-    <div className='flex justify-between items-center h-[4rem] rounded-md shadow-sm  top-navbar'>
+    <div className='flex justify-between items-center h-[4rem] rounded-md shadow-sm  top-navbar z-10'>
         <div className='p-2 px-6 text-2xl font-bold text-title'>
             Dashboard
         </div>
