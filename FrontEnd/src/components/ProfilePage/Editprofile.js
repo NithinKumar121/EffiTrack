1ecb74052415a7d23686d6e519a22dbf81d0d9d8
muruga21 <murugaperumal21062004@gmail.com>
2024-01-16 20:20:53 +0530

    completed profile page

--- FrontEnd/src/components/ProfilePage/Editprofile.js
+++ FrontEnd/src/components/ProfilePage/Editprofile.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
-import { GITHUB_USERNAME , LEETCODE_USERNAME , CODEFORCES_USERNAME , CODECHEF_USERNAME, ImageUpload } from './Platform.username';
+import { GITHUB_USERNAME , LEETCODE_USERNAME , CODEFORCES_USERNAME , CODECHEF_USERNAME, ImageUpload, PROFILE_TOP, LEETCODE_EDIT, USERNAME_EDIT, CODEFORCES_EDIT, CODECHEF_EDIT, GITHUB_EDIT } from './Platform.username';
 
 
 const EditProfile = () => {
     return(
       <>
-        <div className=''> 
-            <ImageUpload/>
-            <LEETCODE_USERNAME/>
-            <CODEFORCES_USERNAME/>
-            <CODECHEF_USERNAME/>
-            <GITHUB_USERNAME/>  
+        <PROFILE_TOP/>
+        <div className='flex w-full justify-center items-center pl-[5rem]'> 
+            <div className='w-full max-w-[60rem] mt-10 p-5 rounded-lg shadow-xl'>
+              {/* <ImageUpload/> */}
+              <USERNAME_EDIT/>
+              <LEETCODE_EDIT/>
+              <CODEFORCES_EDIT/>
+              <CODECHEF_EDIT/>
+              <GITHUB_EDIT/>
+            </div> 
         </div>
       </>
     )
