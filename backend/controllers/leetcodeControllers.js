4432a1c7fcd8ba6ebdbed5648edcb5caf06aa8f9
Cibiyanna <pkcibiyanna@gmail.com>
2024-01-31 20:33:19 +0530

    badges endpoint in backend added

--- backend/controllers/leetcodeControllers.js
+++ backend/controllers/leetcodeControllers.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const {getLeetCount , getLeetRating} = require('../utils/LeetcodeFun');
 
 
@@ -44,6 +45,26 @@ const checkLcUsername = async (req,res) =>{
     }
 }
 
+
+const  getLeetcodeBatch = async (req,res) =>{
+    const leetcode = req.user.leetcode;
+    try{
+        const response = await axios.get(`https://alfa-leetcode-api.vercel.app/All_1s_Well/badges`);
+        if(response.data.badgesCount === 0){
+
+            return res.status(202).json({error:true,message:"User doesn't have badges"})
+            // 204 no content
+        }
+        return  res.status(200).json({error:false,message:response.data});
+        // 200 success
+    }   
+    catch(err){
+        return res.status(504).json({error:true,message:err.message});
+        // 504 This error response is given when the server is acting as a gateway and cannot get a response in time.
+    }
+}
+
+
 module.exports = {
-    getLcCount,getLcRating,checkLcUsername
+    getLcCount,getLcRating,checkLcUsername , getLeetcodeBatch
 }
--- backend/routes/leetcodeRoutes.js
+++ backend/routes/leetcodeRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {getLcCount,getLcRating,checkLcUsername} = require('../controllers/leetcodeControllers');
+const {getLcCount,getLcRating,checkLcUsername,getLeetcodeBatch} = require('../controllers/leetcodeControllers');
 const {auth}  = require('../middleware/authMiddleware')
+
+
 router.route('/count').get(auth,getLcCount)
 router.route('/rating').get(auth,getLcRating);
 router.route('/exist').post(auth,checkLcUsername);
+router.route('/batches').get(auth,getLeetcodeBatch);
+
+
 module.exports = router;
